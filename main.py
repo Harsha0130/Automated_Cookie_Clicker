b7ef27523ec6ad49d25c2d498c93683cf2b25f68
Harsha <harshahm0130@gmail.com>
2024-07-22 21:26:17 +0530

    Getting the item prices for cookie upgrades

--- main.py
+++ main.py
@@ -8,3 +8,30 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
+# Get cookie to click on.
+cookie = driver.find_element(by=By.ID, value="cookie")
+
+# Get upgrade item ids.
+items = driver.find_elements(by=By.CSS_SELECTOR, value="#store div")
+item_ids = [item.get_attribute("id") for item in items]
+
+timeout = time.time() + 5
+five_min = time.time() + 60*5  # 5 minutes
+
+while True:
+    cookie.click()
+
+    # Every 5 seconds:
+    if time.time() > timeout:
+        # Get all upgrade <b> tags
+        all_prices = driver.find_elements(by=By.CSS_SELECTOR, value="#store b")
+        item_prices = []
+
+        # Convert <b> text into an integer price.
+        for price in all_prices:
+            element_text = price.text
+            if element_text != "":
+                cost = int(element_text.split("-")[1].strip().replace(",", ""))
+                item_prices.append(cost)
+
+
